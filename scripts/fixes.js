bdd300557252b7f794c796ec9537eed6f639de26
solarkraft <1337470+solarkraft@users.noreply.github.com>
2021-09-13 21:52:24 +0200

    Hide browser suggestions from search bar
    Otherwise overlap the search suggestions

--- scripts/fixes.js
+++ scripts/fixes.js
@@ -1,3 +1,7 @@
 // Prevent empty search popup from showing up on load
 let searchPopup = document.querySelector("#qsearch__out");
 searchPopup.style.display = "none";
+
+// Hide browser suggestions from search bar that overlap the search suggestions
+let searchBar = document.querySelector("#qsearch__in");
+searchBar.autocomplete = "off";
