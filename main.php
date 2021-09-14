7ef39ca82c345878d66312aca17d8918a7520c9a
solarkraft <1337470+solarkraft@users.noreply.github.com>
2021-09-14 09:10:17 +0200

    Support SVG wiki logos

--- main.php
+++ main.php
@@ -62,7 +62,7 @@ $showIcon = tpl_getConf('showIcon');
 						<?php
 						// get logo either out of the template images folder or data/media folder
 						$logoSize = array();
-						$logo = tpl_getMediaFile(array(':wiki:logo.png', ':logo.png', 'images/logo.png', ':wiki:dokuwiki-128.png'), false, $logoSize);
+						$logo = tpl_getMediaFile(array(':logo.svg', ':wiki:logo.svg', 'images/logo.svg', ':wiki:logo.png', ':logo.png', 'images/logo.png', ':wiki:dokuwiki-128.png'), false, $logoSize);
 						// display logo and wiki title in a link to the home page
 						tpl_link(
 							wl(),
