4fb4b0980e7dbb64ca4d01956ea37c246eebc730
solarkraft <1337470+solarkraft@users.noreply.github.com>
2021-09-14 20:29:20 +0200

    Enable visual editor on Swup navigation

--- scripts/page-transitions.js
+++ scripts/page-transitions.js
@@ -1,1 +1,18 @@
 const swup = new Swup();
+
+// Start Visual Editor
+function setupVisualEditor() {
+    initializeProsemirror();
+    window.proseMirrorIsActive = false;
+
+    if (jQuery('#dw__editform').find('[name=prosemirror_json]').length) {
+        handleEditSession();
+    }
+
+    jQuery(window).on('fastwiki:afterSwitch', function(evt, viewMode, isSectionEdit, prevViewMode) {
+        if (viewMode === 'edit' || isSectionEdit) {
+            handleEditSession();
+        }
+    });
+}
+swup.on("contentReplaced", setupVisualEditor);
