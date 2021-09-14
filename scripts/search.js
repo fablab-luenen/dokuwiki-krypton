57aca1cef92625c2e08a169343cd0c8bb5f54f3c
solarkraft <1337470+solarkraft@users.noreply.github.com>
2021-09-14 02:34:13 +0200

    Add basic page creation option

--- scripts/search.js
+++ scripts/search.js
@@ -6,17 +6,19 @@ searchPopup.style.display = "none";
 let searchBar = document.querySelector("#qsearch__in");
 searchBar.autocomplete = "off";
 
-
 /////// Common
 var searchPopupResults;
+var searchTerm;
 const searchPopupObserver = new MutationObserver((event) => {
+    createPageItem();
+
     searchPopupResults = Array.from(document.querySelectorAll("#qsearch__out ul li a"));
     highlightActiveResult(activeResultId);
 });
 searchPopupObserver.observe(searchPopup, {childList: true});
 
 searchBar.addEventListener("keydown", function(event){
-    keyboardNavigationKeyDown(event.key);
+    keyboardNavigation(event.key);
 });
 
 /////// Keyboard navigation
@@ -61,3 +63,21 @@ function keyboardNavigation(key) {
 
     activeResultId = searchPopupResults[activeIndex].attributes["data-wiki-id"].value;
     highlightActiveResult(activeResultId);
+}
+
+/////// Page creation
+function createPageItem() {
+    console.log("results", searchPopupResults);
+    if(!searchPopupResults) return;
+
+    let pageName = searchBar.value;
+
+    // Do not add this button if the page is already in the list
+    if(searchPopupResults.find(e => e.attributes["data-wiki-id"].value == pageName)) console.log("found");
+
+    let addIcon = '<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><path d="M0 0h24v24H0z" fill="none"/><path d="M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z"/></svg>';
+    
+    // We can't do anything on empty result lists yet, because no suggestion popup will show up. Might fix this in qsearch. 
+    let resultsList = document.querySelector("#qsearch__out ul");
+    resultsList?.insertAdjacentHTML("beforeend", `<li><a href="/${pageName}?do=edit" data-wiki-id="${pageName}">${addIcon} ${pageName} </a></li>`); 
+}
