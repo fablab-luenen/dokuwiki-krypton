111c14b5bf50642def2f3821e5814db072d7891b
solarkraft <1337470+solarkraft@users.noreply.github.com>
2021-09-18 19:36:55 +0200

    Add basic dropdown to header
    Thanks https://moderncss.dev/css-only-accessible-dropdown-navigation-menu/

--- main.php
+++ main.php
@@ -109,6 +109,30 @@ $showBackground = tpl_getConf('headerBackgroundImage');
 
 						?>
 
+						<nav aria-label="Main Navigation" class="dropown"> 
+							<ul>
+								<li><a href="#">About</a></li>
+								<li class="dropdown">
+								<!-- aria-expanded needs managed with Javascript -->
+								<button
+									type="button"
+									class="dropdown__title"
+									aria-expanded="false"
+									aria-controls="sweets-dropdown"
+								>
+									Sweets
+								</button>
+								<ul class="dropdown__menu" id="sweets-dropdown">
+									<li><a href="#">Donuts</a></li>
+									<li><a href="#">Cupcakes</a></li>
+									<li><a href="#">Chocolate</a></li>
+									<li><a href="#">Bonbons</a></li>
+								</ul>
+								</li>
+								<li><a href="#">Order</a></li>
+							</ul>
+						</nav>
+
 
 						<li class="nav-item">
 							<div class="search-form">
