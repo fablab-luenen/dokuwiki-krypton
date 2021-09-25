4cbe013b95961a50d727603a60a708b72e118910
solarkraft <1337470+solarkraft@users.noreply.github.com>
2021-09-25 21:42:03 +0200

    User dropdown: Add name

--- main.php
+++ main.php
@@ -114,12 +114,14 @@ $showBackground = tpl_getConf('headerBackgroundImage');
 								<li class="dropdown">
 									<!-- aria-expanded needs managed with Javascript -->
 									<button
-										aria-expanded="false"
-										title="<?= $_SERVER['REMOTE_USER'] ?>">
+										aria-expanded="false">
 										<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="2 2 22 22" width="24px" fill="currentColor"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 3c1.66 0 3 1.34 3 3s-1.34 3-3 3-3-1.34-3-3 1.34-3 3-3zm0 14.2c-2.5 0-4.71-1.28-6-3.22.03-1.99 4-3.08 6-3.08 1.99 0 5.97 1.09 6 3.08-1.29 1.94-3.5 3.22-6 3.22z"/></svg>
 									</button>
 
 									<ul>
+										<?php if(!empty($_SERVER['REMOTE_USER'])): ?>
+											<li class="user_info"><?php tpl_userinfo(); ?></li>
+										<?php endif; ?>
 										<?php foreach((new \dokuwiki\Menu\UserMenu())->getItems() as $item): ?>
 											<li title="<?= $item->getTitle() ?>">
 												<a href="<?= $item->getLink() ?>">
